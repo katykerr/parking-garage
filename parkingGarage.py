c2b7a0e8aa84c16c0e973f38fca67f75cacf8d3c
Katy Davis <kattykerr@gmail.com>
2022-10-02 14:11:59 -0500

    katy update

---
+++ parkingGarage.py
@@ -0,0 +1,43 @@
+
+# take a ticket
+# dictionary for tickets -1 each time
+# list for space -1 each time
+# one function for both actions
+
+
+
+
+class parking_Garage():
+
+    ticket_available[9]
+    space_available[9]
+
+    def TakeTickets():
+        input("Welcome to the parking garage. Type PARK to take a ticket")
+        if input == "PARK":
+            ticket_list.remove(-1)
+            parking_list.remove(-1)
+        else:
+            print("If you're not parking, please get out.")
+
+
+    def payForParking():
+        input("Ready to leave? Enter your payment total now:")
+        if input:
+            input == dict[value == True]
+            print("Thanks for paying! You have 15 minutes to leave before you self destruct.")
+        else:
+            print("Please pay.")
+
+    def leaveGarage():
+        if dict[value] == True:
+            print("Thank you, have a nice day!")
+
+        else:
+            input("Ready to leave? Enter your payment total now:")
+
+        elif input:
+            input == dict[value == True]
+            ticket_list.append(+1)
+            parking_list.append(+1)
+            print("Thank you, have a nice day!")
