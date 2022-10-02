0291b19f23caf9e96c50192a2d7d2abb70a92771
Katy Davis <kattykerr@gmail.com>
2022-10-02 16:34:23 -0500

    deleted duplicates

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,11 +1,16 @@
 
-# take a ticket
-# dictionary for tickets -1 each time
-# list for space -1 each time
-# one function for both actions
 
-<<<<<<< main
-class Ticket():
+
+class parking_Garage():
+
+     def __init__(self):
+        self.Tickets = []
+        self.Parkingspace = 10
+        self.Currentticket = 0
+
+# ATTRIBUTES:
+
+    class Ticket():
     tickets = []
 
     def __init__(self, paid, price, hours=""):
@@ -45,60 +50,8 @@ class Currentticket():
         self.name = name
         self.confirmation_of_payment = confirmation_of_payment
         print(self.name + " this is your " + self.confirmation_of_payment + ".")
-=======
 
-# take a ticket
-# dictionary for tickets -1 each time
-# list for space -1 each time
-# one function for both actions
-
-
-
-
-class parking_Garage():
-
-    ticket_available[9]
-    space_available[9]
-
-    def TakeTickets():
-        input("Welcome to the parking garage. Type PARK to take a ticket")
-        if input == "PARK":
-            ticket_list.remove(-1)
-            parking_list.remove(-1)
-        else:
-            print("If you're not parking, please get out.")
-
-
-    def payForParking():
-        input("Ready to leave? Enter your payment total now:")
-        if input:
-            input == dict[value == True]
-            print("Thanks for paying! You have 15 minutes to leave before you self destruct.")
-        else:
-            print("Please pay.")
-
-    def leaveGarage():
-        if dict[value] == True:
-            print("Thank you, have a nice day!")
-
-        else:
-            input("Ready to leave? Enter your payment total now:")
-
-        elif input:
-            input == dict[value == True]
-            ticket_list.append(+1)
-            parking_list.append(+1)
-            print("Thank you, have a nice day!")
-
-
-class Parking_Garage:
-
- def __init__(self):
-        self.tickets = []
-        self.parkingSpaces = 10
-        self.currentTicket = 0
-
-
+# METHODS:
 
     def TakeTickets():
         if garage_prompts.lower == "park":
